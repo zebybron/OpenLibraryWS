df2ac0c98fb48d4b9b57b1c18d3231a5f815685a
enjolys <enjolys@IUTCLINFB1816>
2023-09-28 10:48:08 +0200

    stub + dbb

--- Sources/OpenLibraryWrapper/Program.cs
+++ Sources/OpenLibraryWrapper/Program.cs
@@ -2,17 +2,26 @@
 using DtoAbstractLayer;
 using LibraryDTO;
 using Microsoft.OpenApi.Models;
+using MyLibraryDB;
 using MyLibraryManager;
 using OpenLibraryClient;
 using StubbedDTO;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var dbDatabase = System.Environment.GetEnvironmentVariable("DB_DATABASE", System.EnvironmentVariableTarget.Process);
+var dbDatabase = Environment.GetEnvironmentVariable("DB_DATABASE");
+if ( dbDatabase.Equals("stub"))
+{
+    builder.Services.AddSingleton<IDtoManager, Stub>();
+}
+ else
+{
+    builder.Services.AddSingleton<IDtoManager, MyLibraryMgr>();
+}
+
+
 
 // Add services to the container.
-
-builder.Services.AddSingleton<IDtoManager, dbDatabase>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
